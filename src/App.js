a66958d989baaaaf4adff5645cc3dad69df080c4
sotiboldiyevxurshid <sotiboldiyevxurshid21@gmail.com>
2023-04-06 18:59:52 +0500

    cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+
+import Navbar from "./components/Navbar";
+import Amazon from "./components/Amazon";
+import Cart from "./components/Cart";
+
+const App = () => {
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
+
+  const handleClick = (item) => {
+    if (cart.indexOf(item) !== -1) return;
+    setCart([...cart, item]);
+  };
+
+  const handleChange = (item, d) => {
+    const ind = cart.indexOf(item);
+    const arr = cart;
+    arr[ind].amount += d;
+
+    if (arr[ind].amount === 0) arr[ind].amount = 1;
+    setCart([...arr]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar setShow={setShow} size={cart.length} />
+      {show ? (
+        <Amazon handleClick={handleClick} />
+      ) : (
+        <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
+      )}
+    </React.Fragment>
   );
-}
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,44 @@
+const list = [
+  {
+    id: 1,
+    title: "Iphone 13",
+    author: "2018",
+    price: 1000,
+    img: "https://openshop.uz/uploads/products/photos/202201/X0dvZB7kUDESXAbPJ10YxeQ7LqiF0QA3WAsKzgPH.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "Mac Pro",
+    author: "2018",
+    price: 1100,
+    img: "https://assets.asaxiy.uz/product/items/desktop/0584ce565c824b7b7f50282d9a19945b2022082016501324450YXZ9hmd7Aq.png.webp",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Airpods",
+    author: "2018",
+    price: 100,
+    img: "https://cdn.macbro.uz/macbro/830b3232-2ce6-4054-a3ef-e2e3e921cfe6",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "Ipad",
+    author: "2018",
+    price: 700,
+    img: "https://openshop.uz/public/storage/uploads/products/photos/202301/7QZzZzx2KsBeXelxyplQAurftubLuqkqqEm1iOYQ.jpg",
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: "Apple watch",
+    author: "2018",
+    price: 200,
+    img: "https://kattabozor.s3.eu-central-1.amazonaws.com/ri/450ed390b2dbbe91c8b78082bed0aae4a690adeb6384174e0c02834f954d844a_8w0LAy_480l.jpg",
+    amount: 1,
+  },
+];
+
+export default list;
