122d7da4b0748a3a289da6e019821e78d12dac79
sotiboldiyevxurshid <sotiboldiyevxurshid21@gmail.com>
2023-04-08 07:00:49 +0500

    cart

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
-import React, { useState } from "react";
 
-
-import Navbar from "./components/Navbar";
-import Amazon from "./components/Amazon";
-import Cart from "./components/Cart";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import NavbarRouter from "./components/NavbarRouter/NavbarRouter";
+import Section from "./components/Section/Section";
 
 const App = () => {
-  const [show, setShow] = useState(true);
-  const [cart, setCart] = useState([]);
-
-  const handleClick = (item) => {
-    if (cart.indexOf(item) !== -1) return;
-    setCart([...cart, item]);
-  };
-
-  const handleChange = (item, d) => {
-    const ind = cart.indexOf(item);
-    const arr = cart;
-    arr[ind].amount += d;
-
-    if (arr[ind].amount === 0) arr[ind].amount = 1;
-    setCart([...arr]);
-  };
+  
 
   return (
-    <React.Fragment>
-      <Navbar setShow={setShow} size={cart.length} />
-      {show ? (
-        <Amazon handleClick={handleClick} />
-      ) : (
-        <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
-      )}
-    </React.Fragment>
+   <>
+
+
+   
+  <NavbarRouter/>
+  <Header/>
+  <Section/>
+  <Footer/>
+   </>
   );
 };
 
