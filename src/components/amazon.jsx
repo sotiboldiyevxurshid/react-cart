a1876871ebde93ad20a774613cf5744f49bc28cb
sotiboldiyevxurshid <sotiboldiyevxurshid21@gmail.com>
2023-04-11 19:09:11 +0500

    v

--- src/components/amazon.jsx
+++ src/components/amazon.jsx
@@ -5,32 +5,33 @@ import "../styles/amazon.css";
 import { Container, Row } from "react-bootstrap";
 
 const Amazon = ({ handleClick }) => {
-
-const[get,setGet] = useState("")
+  const [get, setGet] = useState("");
 
   return (
-   <>
-  <Container>
-    <Row>
-    <div> 
-      <input  onChange={(e) => setGet(e.target.value)} className="f" type="text" />
-   </div>
-    <section>
-     
-      {list.filter((item) => {
+    <>
+      <Container>
+        <Row>
+          <div>
+            <input
+              onChange={(e) => setGet(e.target.value)}
+              className="f"
+              type="text"
+            />
+          </div>
+          <section>
+            {list
+              .filter((item) => {
                 return get.toLowerCase() === ""
                   ? item
                   : item.title.toLowerCase().includes(get);
               })
               .map((item) => (
-     
-        <Cards key={item.id} item={item} handleClick={handleClick} />
-    
-      ))}
-    </section>
-    </Row>
-  </Container>
-   </>
+                <Cards key={item.id} item={item} handleClick={handleClick} />
+              ))}
+          </section>
+        </Row>
+      </Container>
+    </>
   );
 };
 
